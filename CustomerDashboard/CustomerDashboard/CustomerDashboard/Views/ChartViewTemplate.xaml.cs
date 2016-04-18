4bfdf24b697805cac53e1ec086bb4b76e14e8173
Yoshito Tabuchi on x220 <ytabuchi.xlsoft@gmail.com>
2016-04-18 19:07:26 +0900

    First Commit

---
+++ CustomerDashboard/CustomerDashboard/CustomerDashboard/Views/ChartViewTemplate.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace CustomerDashboard.Views
+{
+    public partial class ChartViewTemplate : ContentView
+    {
+        public ChartViewTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+}
